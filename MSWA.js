024089ef4ccebf83a98a1dcfd362eea100690afa
Misael <110029376+Misael-Mods@users.noreply.github.com>
2022-07-26 08:13:35 -0500

    Create MSWA.js

---
+++ MSWA.js
@@ -0,0 +1,18 @@
+{
+	"version": "2.0",
+	"download": "https://t.me/MsWhatsApp",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/Misael-Mods/MSWA/main/changelog-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/Misael-Mods/MSWA/main/changelog-es.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/Misael-Mods/MSWA/main/changelog-pt.html"
+		}
+	]
+}
